e3807ed6899bd33c0036d8687366b19c2aef22e0
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-02-12 13:54:42 +0700

    tugas 2 (pengayaan) jobsheet 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,18 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
-      <h1>Praktikum Next.js Pages Router</h1> <br />
+      <nav>
+        {/* Prefetched when the link is hovered or enters the viewport */}
+        <Link href="/about">About </Link>
+      </nav>
+      <h1>Praktikum Next.js Pages Router</h1>
       <p>Mahasiswa D4 Pengembangan Web</p>
     </div>
   );
