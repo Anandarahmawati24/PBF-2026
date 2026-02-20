58a9ee557113d6c40b118e1028eb24102b0eea0b
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-02-20 13:40:58 +0700

    Langkah 5 membuat komponen navbar di semua halaman jobsheet 3

--- Pertemuan_3/src/pages/index.tsx
+++ Pertemuan_3/src/pages/index.tsx
@@ -1,11 +1,9 @@
 import { Inter } from "next/font/google";
-import Navbar from "@/components/layouts/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Praktikum Next.js Pages Router</h1> <br />
       <p>Mahasiswa D4 Pengembangan Web</p>
     </div>
