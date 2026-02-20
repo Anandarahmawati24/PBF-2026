06c65b4f22e62a9aa54c03eff6055a03315852d9
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-02-20 11:52:55 +0700

    Langkah 4 Dynamic Routing bagian b& c jobsheet 3

--- Pertemuan_3/src/pages/produk/[id].tsx
+++ Pertemuan_3/src/pages/produk/[id].tsx
@@ -1,16 +1,14 @@
 import { useRouter } from "next/router";
 
 const HalamanProduk = () => {
-  const router = useRouter();
-  const { id } = router.query;
-
-  console.log("Router:", router);
-  console.log("ID Produk:", id);
+ // const router = useRouter();
+ // console.log(Router);
+   const { query } = useRouter();
 
   return (
     <div>
       <h2>Halaman Produk</h2>
-      <p>Produk: {id}</p>
+      <p>Produk: {query.id}</p>
     </div>
   );
 };
