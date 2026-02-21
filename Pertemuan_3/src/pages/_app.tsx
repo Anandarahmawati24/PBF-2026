ee115bc020beffe5139a612078a33ac4c737a457
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-02-21 13:29:45 +0700

    Membuat Layout Global jobsheet 3

--- Pertemuan_3/src/pages/_app.tsx
+++ Pertemuan_3/src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import Navbar from '@/components/layouts/navbar'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Appshell from "@/components/layouts/Appshell";
+import Navbar from "@/components/layouts/navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <Navbar />
-       <Component {...pageProps} />
-    </div>
-    );
-};
+    <Appshell>
+      <Component {...pageProps} />
+    </Appshell>
+  );
+}
