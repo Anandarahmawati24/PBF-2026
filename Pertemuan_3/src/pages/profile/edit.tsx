8de2ca653433cc3b34b09d631c48fb6313116137
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-02-21 13:31:19 +0700

    Tugas 1 Routing Jobsheet 3

--- Pertemuan_3/src/pages/profile/edit.tsx
+++ Pertemuan_3/src/pages/profile/edit.tsx
@@ -2,7 +2,7 @@ const ProfileEditPage = () => {
   return (
     <div>
       <h1>User Profile Edit Page</h1>
-      <p>Ini adalah halaman edit profil pengguna.</p>
+      <p>Ini adalah halaman edit profil pengguna</p>
     </div>
   );
 };
