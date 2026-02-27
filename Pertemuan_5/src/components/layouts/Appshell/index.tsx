40672758ed884499a2670a3ffb968612b45f46b9
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-02-27 13:23:52 +0700

    Langkah 4 Conditional Rendering Navbar Jobsheet 5

--- Pertemuan_5/src/components/layouts/Appshell/index.tsx
+++ Pertemuan_5/src/components/layouts/Appshell/index.tsx
@@ -1,18 +1,20 @@
+import {useRouter} from "next/router";
 import Navbar from "../navbar";
 
+const disableNavbar = ["/auth/login", "/auth/register"];
 type AppshellProps = {
     children: React.ReactNode;
 };
 
 const Appshell = (props: AppshellProps) => {
+    const {pathname} = useRouter();
     const { children } = props;
+    const router = useRouter();
+    console.log(router);
     return(
         <main>
-            <Navbar />
+            {!disableNavbar.includes(pathname) && <Navbar />}
             {children}
-            <div className="footer">
-                Footer
-            </div>
         </main>
     );
 };
