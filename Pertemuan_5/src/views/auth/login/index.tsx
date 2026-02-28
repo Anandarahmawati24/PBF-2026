e3f51a11cf24e6cdc8807c61c94acaa9b4d0f339
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-02-28 13:11:48 +0700

    Langkah 9.	Tailwind CSS jobsheet 5

--- Pertemuan_5/src/views/auth/login/index.tsx
+++ Pertemuan_5/src/views/auth/login/index.tsx
@@ -11,11 +11,14 @@ const TampilanLogin = () => {
   };
   return (
     <div className={styles.login}>
-      <h1>Halaman Login</h1>
+      <h1 className="text-3xl font-bold text-blue-600 ">Halaman Login</h1>
       {/* <button onClick={handlerLogin}>Login</button> <br /> }*/}
       {/* <button onClick={() => push("/produk")}>Login </button> <br /> */}
       <button onClick={() => handlerLogin()}>Login</button> <br />
-      <h1 style={{ color: "red",borderRadius: "10px",padding: "10px",}}> belum punya akun</h1>
+      <h1 style={{ color: "red",border: "1px solid red", borderRadius: "5px", padding: "5px" }}>
+        {" "}
+        belum punya akun
+      </h1>
       <Link href="/auth/register">Ke Halaman Register</Link>
     </div>
   );
