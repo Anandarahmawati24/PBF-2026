d2992095e0cc552f4ff68da39cd1bd1c284a679e
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-02-28 12:00:43 +0700

    Langkah 6.	Inline Styling (CSS-in-JS) jobsheet 5

--- Pertemuan_5/src/views/auth/login/index.tsx
+++ Pertemuan_5/src/views/auth/login/index.tsx
@@ -14,7 +14,8 @@ const TampilanLogin = () => {
       {/* <button onClick={handlerLogin}>Login</button> <br /> }*/}
       {/* <button onClick={() => push("/produk")}>Login </button> <br /> */}
       <button onClick={() => handlerLogin()}>Login</button> <br />
-        <Link href="/auth/register">Ke Halaman Register</Link>
+      <h1 style={{ color: "red",borderRadius: "10px",padding: "10px",}}> belum punya akun</h1>
+      <Link href="/auth/register">Ke Halaman Register</Link>
     </div>
   );
 };
