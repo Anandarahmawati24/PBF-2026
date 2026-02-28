f2c0c6763bb4f50a55a943832e930ba0ac0bbfc4
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-02-28 17:54:11 +0700

    Langkah 6 Menampilkan Gambar dari Folder Public jobsheet 6

--- Pertemuan_6/src/pages/404.tsx
+++ Pertemuan_6/src/pages/404.tsx
@@ -2,6 +2,7 @@ import styles from "@/styles/404.module.scss";
 const Custom404 = () => {
     return (
         <div className={styles.eror}>
+            <img src="/page-not-found.png" alt="404" className={styles.eror_image} />
             <h1>404 - Halaman Tidak Ditemukan</h1>
             <p>Maaf, halaman yang Anda cari tidak ditemukan.</p>
         </div>
