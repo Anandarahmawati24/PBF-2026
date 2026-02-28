15ab221c38516bf02b6af97002ec7ec101c566f8
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-02-28 19:01:42 +0700

    tugas 1 jobsheet 6

--- Pertemuan_6/src/pages/404.tsx
+++ Pertemuan_6/src/pages/404.tsx
@@ -1,7 +1,11 @@
 import styles from "@/styles/404.module.scss";
+import Head from "next/head";
 const Custom404 = () => {
     return (
         <div className={styles.eror}>
+            <head>
+                <title>Error - 404</title>   
+            </head>
             <img src="/page-not-found.png" alt="404" className={styles.eror_image} />
             <h1>404 - Halaman Tidak Ditemukan</h1>
             <p>Maaf, halaman yang Anda cari tidak ditemukan.</p>
@@ -9,4 +13,4 @@ const Custom404 = () => {
     );
 };
 
-export default Custom404;
+export default Custom404
