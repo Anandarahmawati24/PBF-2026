da068c49c459509fc4b1d6fa3d61d595fe162805
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-02-28 11:56:00 +0700

    Langkah 5 Refactoring Struktur Project Jobsheet 5

--- Pertemuan_5/src/pages/auth/login.tsx
+++ Pertemuan_5/src/pages/auth/login.tsx
@@ -1,21 +1,10 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import styles from "./login.module.css";
+import TampilanLogin from "@/views/auth/login";
 
 const halamanLogin = () => {
-    const {push} = useRouter();
-    const handlerLogin = () => {
-     // logic login
-        push("/produk");
-    };
     return (
-        <div className={styles.login}>
-            <h1>Halaman Login</h1>
-           {/* <button onClick={handlerLogin}>Login</button> <br /> }*/}
-           {/* <button onClick={() => push("/produk")}>Login </button> <br /> */}
-            <button onClick={() => handlerLogin()}>Login</button> <br />
-           <p>Belum punya akun? <br /> <Link href="/auth/register">Ke Halaman Register</Link></p>
-        </div>
+        <>
+        <TampilanLogin />   
+        </>
     );
 };
 
