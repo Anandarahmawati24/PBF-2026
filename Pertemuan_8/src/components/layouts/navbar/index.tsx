8e3bb79ef236e2cfd9006da4473ff778a4e101b2
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-03-01 20:02:50 +0700

    navbar

--- Pertemuan_8/src/components/layouts/navbar/index.tsx
+++ Pertemuan_8/src/components/layouts/navbar/index.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
       <div className={styles.navbar}>
         <div className='big'>
-        <h1>Navbar component</h1>
+        <h1>Navbar</h1>
         </div>
       </div>
     );
