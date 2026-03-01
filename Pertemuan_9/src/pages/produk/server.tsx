7f9347ed5eabf09d2398fb722f31ea3caf2163e6
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-03-01 22:45:25 +0700

    Langkah 2 Implementasi getServerSideProps pada server.tsx jobsheet 9

--- Pertemuan_9/src/pages/produk/server.tsx
+++ Pertemuan_9/src/pages/produk/server.tsx
@@ -1,12 +1,32 @@
 import ProdukView from "@/views/produk";
 
-const HalamanProdukServer = () => {
+type ProductType = {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+  category: string;
+};
+
+const HalamanProdukServer = (props:{products: ProductType[]}) => {
   return (
     <div>
       <h1>Halaman Produk Server</h1>
-      <ProdukView products={[]} isLoading={false} />
+      <ProdukView products={props.products} isLoading={false} />
     </div>
   );
 };
 
 export default HalamanProdukServer;
+
+//fungsi getServerSideProps akan dipanggil setiap kali halaman ini diakses, dan akan mengambil data produk dari API sebelum merender halaman.
+export async function getServerSideProps() {
+    const res = await fetch("http://localhost:3000/api/produk");
+    const respone = await res.json();
+    //console.log("Data produk yang diambil dari API:", respone);
+    return {
+      props: {
+        products: respone.data, // pastikan untuk memberikan nilai default jika data tidak tersedia
+      },
+    };
+};
