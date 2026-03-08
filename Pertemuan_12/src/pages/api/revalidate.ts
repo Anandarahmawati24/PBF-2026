2a67d4fd979be6879872e261766448cf95b13adc
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-03-08 16:15:45 +0700

    Bagian 2 – Tambahkan Parameter Data,  On-Demand Revalidation jobsheet 12

--- Pertemuan_12/src/pages/api/revalidate.ts
+++ Pertemuan_12/src/pages/api/revalidate.ts
@@ -3,16 +3,22 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
     revalidated: boolean;
+    message?: string;
 };
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<Data>
+    res: NextApiResponse<Data>,
 ) {
-    try {await res.revalidate("/produk/static");
+    if (req.query.data === "produk") {
+    try {
+        await res.revalidate("/produk/static");
         return res.status(200).json({revalidated: true});
     } catch (error) {
         console.error("Error in API route:", error);
         res.status(500).json({ revalidated: false });
     }
+}
+
+return res.json({revalidated: false, message: "Invalid query parameter. Expected 'data=produk'."});
 }
