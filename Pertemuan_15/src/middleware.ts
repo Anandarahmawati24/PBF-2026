2b3618a4355605461783818732285d27bf60b60f
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-03-30 22:23:13 +0700

    Bagian 1 – Membuat Register View jobsheet 15

---
+++ Pertemuan_15/src/middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import type { NextRequest, NextFetchEvent } from "next/server";
+import withAuth from "./middleware/withAuth";
+
+function middleware(request: NextRequest, event: NextFetchEvent) {
+  return NextResponse.next();
+}
+
+export default withAuth(middleware, ["/profile", "/produk", "/about"]);
+
+export const config = {
+  matcher: ["/profile","/produk","/about"],
+};
