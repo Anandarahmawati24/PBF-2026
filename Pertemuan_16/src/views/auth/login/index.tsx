472958db5796c447fa562c7faac8e2966ef3c5d7
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-04-01 22:19:21 +0700

    solving error

--- Pertemuan_16/src/views/auth/login/index.tsx
+++ Pertemuan_16/src/views/auth/login/index.tsx
@@ -7,7 +7,7 @@ import { signIn } from "next-auth/react";
 const TampilanLogin = () => {
   const [isLoading, setIsLoading] = useState(false);
   const { push, query } = useRouter();
-  const callbackUrl:any = query.callbackUrl || "/";
+  const callbackUrl: any = query.callbackUrl || "/";
   const [error, setError] = useState("");
 
   const handleSubmit = async (event: any) => {
@@ -15,80 +15,78 @@ const TampilanLogin = () => {
     setError("");
     setIsLoading(true);
 
-  //   const form = event.currentTarget;
-  //   const formdata = new FormData(event.currentTarget);
-  //   const email = formdata.get("email") as string;
-  //   const fullName = formdata.get("Fullname") as string;
-  //   const password = formdata.get("Password") as string;
+    //   const form = event.currentTarget;
+    //   const formdata = new FormData(event.currentTarget);
+    //   const email = formdata.get("email") as string;
+    //   const fullName = formdata.get("Fullname") as string;
+    //   const password = formdata.get("Password") as string;
 
-  //   if (!email) {
-  //     setError("Email wajib diisi");
-  //     setIsLoading(false);
-  //     return;
-  //   }
+    //   if (!email) {
+    //     setError("Email wajib diisi");
+    //     setIsLoading(false);
+    //     return;
+    //   }
 
-  //   if (password.length < 6) {
-  //     setError("Password minimal 6 karakter");
-  //     setIsLoading(false);
-  //     return;
-  //   }
+    //   if (password.length < 6) {
+    //     setError("Password minimal 6 karakter");
+    //     setIsLoading(false);
+    //     return;
+    //   }
 
-  //   const response = await fetch("/api/login", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({ email, fullName, password }),
-  //   });
-  //   // const result = await response.json();
-  //   // console.log(result);
-  //   if (response.status === 200) {
-  //     form.reset();
-  //     //event.currentTarget.reset();
-  //     setIsLoading(false);
-  //     push("/");
-  //   } else {
-  //     setIsLoading(false);
-  //     setError(
-  //       response.status === 400
-  //         ? "Email atau password salah. Silakan coba lagi."
-  //         : "An error occurred. Please try again later.",
-  //     );
-  //   }
+    //   const response = await fetch("/api/login", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({ email, fullName, password }),
+    //   });
+    //   // const result = await response.json();
+    //   // console.log(result);
+    //   if (response.status === 200) {
+    //     form.reset();
+    //     //event.currentTarget.reset();
+    //     setIsLoading(false);
+    //     push("/");
+    //   } else {
+    //     setIsLoading(false);
+    //     setError(
+    //       response.status === 400
+    //         ? "Email atau password salah. Silakan coba lagi."
+    //         : "An error occurred. Please try again later.",
+    //     );
+    //   }
 
-  try {
-    const res = await signIn("credentials", {
-      redirect: false,
-      email: event.target.email.value,
-      password: event.target.Password.value,
-      callbackUrl,
-    });
+    try {
+      const res = await signIn("credentials", {
+        redirect: false,
+        email: event.target.email.value,
+        password: event.target.password.value,
+        callbackUrl,
+      });
 
-    //console.log("signIn response:", res);
-    if (res?.error) {
-      setIsLoading(false);
-      push(callbackUrl);
-    } else {
-      setIsLoading(false);
-      setError(res?.error || "Login failed");
-    }
+      //console.log("signIn response:", res);
+      if (res?.error) {
+        setIsLoading(false);
+        setError("wrong email or password. Please try again.");
+      } else {
+        setIsLoading(false);
+        push(callbackUrl);
+      }
     } catch (error) {
       setIsLoading(false);
-      setError("wrong email or password");
-  }
-   };
+      setError("An error occurred. Please try again later.");
+    }
+  };
 
   return (
+    <>
     <div className={style.login}>
       {error && <p className={style.login__error}>{error}</p>}
       <h1 className={style.login__title}>Halaman Login</h1>
       <div className={style.login__form}>
         <form onSubmit={handleSubmit}>
           <div className={style.login__form__item}>
-            <label
-              htmlFor="email"
-              className={style.login__form__item__label}
-            >
+            <label htmlFor="email" className={style.login__form__item__label}>
               {" "}
               Email
             </label>
@@ -125,10 +123,12 @@ const TampilanLogin = () => {
         </form>
         <br />
         <p className={style.login__form__item__text}>
-          Belum punya akun? <Link href="/auth/register">Ke Halaman Register</Link>
+          Belum punya akun?{" "}
+          <Link href="/auth/register">Ke Halaman Register</Link>
         </p>
       </div>
     </div>
+    </>
   );
 };
 
