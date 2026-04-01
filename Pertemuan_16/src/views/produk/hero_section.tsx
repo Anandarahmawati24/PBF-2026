b961ecee04b04d1a70fac5696900749d9ce9ae20
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-04-01 08:55:28 +0700

    BAGIAN 1 – Custom Login Page jobsheet 16

---
+++ Pertemuan_16/src/views/produk/hero_section.tsx
@@ -0,0 +1,10 @@
+const HeroSection = () => {
+  return (
+    <div className="bg-blue-600 text-white py-5 text-center">
+      <h1 className="text-4xl font-bold mb-4"> Selamat Datang di Halaman Produk</h1>
+      <p className="text-lg">Temukan produk terbaik pilihan Anda</p>
+    </div>
+  );
+};
+
+export default HeroSection;
