3c3134662b6463cfe080bb4f53e134d29cd7bf33
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-04-03 19:09:59 +0700

    tugas nomor 2 Gunakan minimal 1 font dari next/font jobsheet 18

--- Pertemuan_18/src/components/layouts/Appshell/index.tsx
+++ Pertemuan_18/src/components/layouts/Appshell/index.tsx
@@ -1,13 +1,13 @@
 import {useRouter} from "next/router";
 import Navbar from "../navbar";
-import { Roboto } from "next/font/google";
+import { Montserrat } from "next/font/google";
 
 const disableNavbar = ["/auth/login", "/auth/register","/404"];
 type AppshellProps = {
     children: React.ReactNode;
 };
 
-const roboto = Roboto({
+const montserrat = Montserrat({
     subsets: ["latin"],
     weight: ["400", "500", "700"],
 });
@@ -18,7 +18,7 @@ const Appshell = (props: AppshellProps) => {
     //const router = useRouter();
    // console.log(router);
     return(
-        <main className={roboto.className}>
+        <main className={montserrat.className}>
             {!disableNavbar.includes(pathname) && <Navbar />}
             {children}
         </main>
