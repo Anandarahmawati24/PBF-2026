ba0a024c7e495d75f7a869490dbbac43e52dd9f4
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-04-03 16:39:53 +0700

    PRAKTIKUM 2 – Font Optimization jobsheet 18

--- Pertemuan_18/src/components/layouts/Appshell/index.tsx
+++ Pertemuan_18/src/components/layouts/Appshell/index.tsx
@@ -1,18 +1,24 @@
 import {useRouter} from "next/router";
 import Navbar from "../navbar";
+import { Roboto } from "next/font/google";
 
 const disableNavbar = ["/auth/login", "/auth/register","/404"];
 type AppshellProps = {
     children: React.ReactNode;
 };
 
+const roboto = Roboto({
+    subsets: ["latin"],
+    weight: ["400", "500", "700"],
+});
+
 const Appshell = (props: AppshellProps) => {
     const {pathname} = useRouter();
     const { children } = props;
-    const router = useRouter();
-    console.log(router);
+    //const router = useRouter();
+   // console.log(router);
     return(
-        <main>
+        <main className={roboto.className}>
             {!disableNavbar.includes(pathname) && <Navbar />}
             {children}
         </main>
