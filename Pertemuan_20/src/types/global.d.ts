e6052a5c0d35f0582d1152e25ed8bda30ecc684f
anandarahmawati24 <rahmawatiananda571@gmail.com>
2026-04-04 20:04:43 +0700

    fix: typescript errors for vercel deployment

--- Pertemuan_20/src/types/global.d.ts
+++ Pertemuan_20/src/types/global.d.ts
@@ -4,4 +4,12 @@ declare global {
   }
 }
 
-export {};
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: { [className: string]: string };
+  export default content;
+}
